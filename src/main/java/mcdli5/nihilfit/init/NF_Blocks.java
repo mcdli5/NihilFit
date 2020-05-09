1072f8470678a3ad83a010cb5299c0eadf7eab78
Douglas Liberalesso <dliberalesso@gmail.com>
2020-05-09 02:37:26 -0300

    Replace CrushedBlockSetup with an Enum

--- src/main/java/mcdli5/nihilfit/init/NF_Blocks.java
+++ src/main/java/mcdli5/nihilfit/init/NF_Blocks.java
@@ -1,22 +1,22 @@
 package mcdli5.nihilfit.init;
 
 import com.tterrag.registrate.util.RegistryEntry;
+import mcdli5.nihilfit.block.CrushedBlock;
 import mcdli5.nihilfit.block.crucible.CrucibleStoneBlock;
 import mcdli5.nihilfit.block.crucible.CrucibleWoodBlock;
 import net.minecraft.block.FallingBlock;
 
 import static mcdli5.nihilfit.block.crucible.CrucibleSetup.registerStoneCrucible;
 import static mcdli5.nihilfit.block.crucible.CrucibleSetup.registerWoodCrucible;
-import static mcdli5.nihilfit.block.crushedblock.CrushedBlockSetup.registerCrushedBlock;
 
 public final class NF_Blocks {
     // Crushed Blocks
-    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_ANDESITE = registerCrushedBlock("Andesite");
-    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_DIORITE = registerCrushedBlock("Diorite");
-    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_DUST = registerCrushedBlock("Dust");
-    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_END_STONE = registerCrushedBlock("End Stone");
-    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_GRANITE = registerCrushedBlock("Granite");
-    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_NETHERRACK = registerCrushedBlock("Netherrack");
+    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_ANDESITE = CrushedBlock.ANDESITE.getRegistryEntry();
+    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_DIORITE = CrushedBlock.DIORITE.getRegistryEntry();
+    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_DUST = CrushedBlock.DUST.getRegistryEntry();
+    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_END_STONE = CrushedBlock.END_STONE.getRegistryEntry();
+    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_GRANITE = CrushedBlock.GRANITE.getRegistryEntry();
+    public static final RegistryEntry<FallingBlock> CRUSHEDBLOCK_NETHERRACK = CrushedBlock.NETHERRACK.getRegistryEntry();
 
     // Crucibles
     public static final RegistryEntry<CrucibleStoneBlock> CRUCIBLE_STONE = registerStoneCrucible();
